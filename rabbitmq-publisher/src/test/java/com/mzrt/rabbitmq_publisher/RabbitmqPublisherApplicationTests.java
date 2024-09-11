96f42333265551bf09037cc62f61171ce996beb5
Stiven Medina <stvnm33@gmail.com>
2024-09-11 15:19:18 -0500

    Innit commit

---
+++ rabbitmq-publisher/src/test/java/com/mzrt/rabbitmq_publisher/RabbitmqPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mzrt.rabbitmq_publisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqPublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
